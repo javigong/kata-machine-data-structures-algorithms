f252c1a66eedfbb725a65469078b5d8a988f15ec
Javier Gongora <jgongora@gmail.com>
2022-09-21 15:36:17 -0700

    Add stack data structure

--- src/day1/Stack.ts
+++ src/day1/Stack.ts
@@ -1,18 +1,48 @@
+type Node<T> = {
+    value: T;
+    prev?: Node<T>;
+};
+
 export default class Stack<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
 
     constructor() {
+        this.head = undefined;
+        this.length = 0;
     }
 
     push(item: T): void {
+        const node = {value: item} as Node<T>;
 
-}
+        this.length++;
+
+        if (!this.head) {
+            this.head = node;
+            return;
+        }
+
+        node.prev = this.head; // new node prev points to head
+        this.head = node; // update head pointing to new node
+    }
+
     pop(): T | undefined {
+        this.length = Math.max(0, this.length - 1);
+        if (this.length === 0) {
+            const head = this.head as Node<T>;
+            this.head = undefined;
+            return head?.value;
+        }
 
-}
+        const head = this.head as Node<T>; // save head
+        this.head = head.prev // update new head
+
+        // free
+
+        return head.value;
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
