eb87a293c5d936ed9a2176492048a37cdf9e5006
Javier Gongora <jgongora@gmail.com>
2022-09-21 15:35:49 -0700

    Add queue data structure

--- src/day1/Queue.ts
+++ src/day1/Queue.ts
@@ -1,18 +1,50 @@
+type Node<T> = {
+    value: T;
+    next?: Node<T>;
+};
+
 export default class Queue<T> {
     public length: number;
-
-    
+    private head?: Node<T>;
+    private tail?: Node<T>;
 
     constructor() {
+        this.head = this.tail = undefined;
+        this.length = 0;
     }
 
     enqueue(item: T): void {
+        const node = {value: item} as Node<T>; // create new node
+        this.length++;
+        if (!this.tail) { // if tail is undefined add new node as tail and head
+            this.tail = this.head = node;
+            return;
+        }
 
-}
+        this.tail.next = node; // add to current tail next to the new node
+        this.tail = node; // update new tail pointing to the new node;
+    }
+
     deque(): T | undefined {
+        if (!this.head) {
+            return undefined;
+        }
 
-}
+        this.length--;
+        if (this.length === 0) {
+            this.tail = undefined;
+        }
+
+        const head = this.head; // save the head
+        this.head = this.head.next; // update the head
+
+        // free (for some lang without garbage collector)
+        head.next = undefined;
+        
+        return head.value; // return head value
+    }
+
     peek(): T | undefined {
-
-}
+        return this.head?.value;
+    }
 }
