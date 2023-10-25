b2982ac207d01bc6faf6d59976d6c29d92475ef2
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-25 15:59:24 -0700

    Add inorder binary try traversal

--- src/day1/BTInOrder.ts
+++ src/day1/BTInOrder.ts
@@ -1,3 +1,24 @@
-export default function in_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): void {
+    // base case
+    if (!curr) {
+        return;
+    }
 
+    // recurse
+    // pre
+    
+    // recurse
+    walk(curr.left, path);
+    path.push(curr.value);
+    walk(curr.right, path);
+
+    // post
+}
+
+export default function in_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+
+    walk(head, path);
+
+    return path;
 }
