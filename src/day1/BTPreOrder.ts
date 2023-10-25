b7112b43cfd013ffc636698ca63689dbf73336df
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-25 15:58:39 -0700

    Add preorder binary tree traversal

--- src/day1/BTPreOrder.ts
+++ src/day1/BTPreOrder.ts
@@ -1,3 +1,24 @@
-export default function pre_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): void {
+    // base case
+    if (!curr) {
+        return;
+    }
 
+    // recurse
+    // pre
+    path.push(curr.value);
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+}
+
+export default function pre_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+    
+    walk(head, path);
+
+    return path;
 }
