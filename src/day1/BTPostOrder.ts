5a6a7b0f4436f63ebef4b06a9907261f72696777
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-25 16:00:52 -0700

    Add postorder binary tree traversal

--- src/day1/BTPostOrder.ts
+++ src/day1/BTPostOrder.ts
@@ -1,3 +1,24 @@
-export default function post_order_search(head: BinaryNode<number>): number[] {
+function walk(curr: BinaryNode<number> | null, path: number[]): void {
+    // base case
+    if (!curr) {
+        return;
+    }
 
+    // recurse
+    // pre
+
+    // recurse
+    walk(curr.left, path);
+    walk(curr.right, path);
+
+    // post
+    path.push(curr.value);
+}
+
+export default function post_order_search(head: BinaryNode<number>): number[] {
+    const path: number[] = [];
+
+    walk(head, path);
+
+    return path;  
 }
