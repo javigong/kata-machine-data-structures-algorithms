3768c16e3a1e1a5c78da38c0cc591fc5f2b02232
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-25 22:11:01 -0700

    Add depth first search to compare binary trees

--- src/day1/CompareBinaryTrees.ts
+++ src/day1/CompareBinaryTrees.ts
@@ -1,3 +1,20 @@
-export default function compare(a: BinaryNode<number> | null, b: BinaryNode<number> | null): boolean {
+export default function compare(
+    a: BinaryNode<number> | null,
+    b: BinaryNode<number> | null,
+): boolean {
+    // base case with all comparisons being true
+    // it's only met when going all down to the bottom doing all the recursions
+    if (a === null && b === null) {
+        return true;
+    }
+    // base cases of comparison being false
+    if (a === null || b === null) {
+        return false;
+    }
+    if (a.value !== b.value) {
+        return false;
+    }
 
+    // recursion
+    return compare(a.left, b.left) && compare(a.right, b.right);
 }
