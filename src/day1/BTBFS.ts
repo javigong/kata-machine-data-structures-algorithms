bb356ccb211466344aeca6d05e3366bb1108d07c
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-25 21:37:02 -0700

    Update bt breadth first search

--- src/day1/BTBFS.ts
+++ src/day1/BTBFS.ts
@@ -1,21 +1,28 @@
 export default function bfs(head: BinaryNode<number>, needle: number): boolean {
-    const q = [head];
+    // queue
+    const q: (BinaryNode<number> | null | undefined)[] = [head];
 
+    // dequeue/shift node from the queue, visit node/search, enqueue its children 
     while (q.length) {
-        const curr = q.shift() as BinaryNode<number>;
+        // const curr = q.shift() as BinaryNode<number>;
 
-        // search
+        // dequeue node from the queue
+        const curr = q.shift();
+
+        // if null dequeue the next node
+        if (!curr) {
+          continue;
+        }
+
+        // visit node/search
         if (curr.value === needle) {
             return true;
         }
 
-        if (curr.left) {
-            q.push(curr.left);
-        }
-
-        if (curr.right) {
-            q.push(curr.right);
-        }
+        // enqueue node's children
+        q.push(curr.left);
+        q.push(curr.right);
+        
     }
 
     return false;
