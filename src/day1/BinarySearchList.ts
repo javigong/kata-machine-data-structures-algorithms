a14372be03ec6c58197ec480b701c74badf705f1
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-26 16:32:21 -0700

    Update BinarySearchList

--- src/day1/BinarySearchList.ts
+++ src/day1/BinarySearchList.ts
@@ -1,19 +1,19 @@
 export default function bs_list(haystack: number[], needle: number): boolean {
-  let lo = 0;
-  let hi = haystack.length;
+    let left = 0;
+    let right = haystack.length;
+    // O(log n)
+    do {
+        const midpoint = Math.floor((left + right) / 2);
+        const currValue = haystack[midpoint];
 
-  do {
-      const m = Math.floor(lo + (hi - lo) / 2);
-      const v = haystack[m];
+        if (currValue === needle) {
+            return true;
+        } else if (currValue < needle) {
+            left = midpoint + 1;
+        } else {
+            right = midpoint - 1;
+        }
+    } while (left < right);
 
-      if (v === needle) {
-          return true;
-      } else if (v > needle) {
-          hi = m;
-      } else {
-          lo = m + 1;
-      }
-  } while (lo < hi);
-
-  return false;
+    return false;
 }
