89ddd7fc5c49eaa2c485ee8ba7231e7fa11ed191
Javier Gongora <42308135+javigong@users.noreply.github.com>
2023-10-30 15:32:07 -0700

    Add MinHeap

--- src/day1/MinHeap.ts
+++ src/day1/MinHeap.ts
@@ -1,15 +1,87 @@
 export default class MinHeap {
     public length: number;
-
-    
+    private data: number[];
 
     constructor() {
+        this.length = 0;
+        this.data = [];
+    }
+    // O(log n)
+    insert(value: number): void {
+        this.data[this.length] = value;
+        this.heapifyUp(this.length);
+        this.length++;
     }
 
-    insert(value: number): void {
-
-}
+    // O(log n)
     delete(): number {
+        if (this.length === 0) {
+            return -1;
+        }
 
-}
+        const out = this.data[0];
+        this.length--;
+
+        if (this.length === 0) {
+            this.data = [];
+            return out;
+        }
+
+        this.data[0] = this.data[this.length];
+        this.heapifyDown(0);
+        return out;
+    }
+
+    private heapifyDown(idx: number): void {
+        const leftIdx = this.leftChild(idx);
+        const rightIdx = this.rightChild(idx);
+
+        if (idx >= this.length || leftIdx >= this.length) {
+            return;
+        }
+
+        const leftValue = this.data[leftIdx];
+        const rightValue = this.data[rightIdx];
+        const value = this.data[idx];
+
+        if (leftValue > rightValue && value > rightValue) {
+            this.data[idx] = rightValue;
+            this.data[rightIdx] = value;
+            this.heapifyDown(rightIdx);
+        } else if (rightValue > leftValue && value > leftValue) {
+            this.data[idx] = leftValue;
+            this.data[leftIdx] = value;
+            this.heapifyDown(leftIdx);
+        }
+    }
+
+    private heapifyUp(idx: number): void {
+        if (idx === 0) {
+            return;
+        }
+
+        const parentIdx = this.parent(idx);
+        const parentValue = this.data[parentIdx];
+        const value = this.data[idx];
+
+        if (parentValue > value) {
+            // swap
+            this.data[idx] = parentValue;
+            this.data[parentIdx] = value;
+            // recurse
+            this.heapifyUp(parentIdx);
+        }
+    }
+
+    private parent(idx: number): number {
+        return Math.floor((idx - 1) / 2);
+    }
+
+    private leftChild(idx: number): number {
+        return idx * 2 + 1;
+    }
+
+    private rightChild(idx: number): number {
+        return idx * 2 + 2;
+    }
 }
